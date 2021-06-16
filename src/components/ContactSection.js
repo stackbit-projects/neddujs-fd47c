6b6cb8b2c6fced785722158eb2ea18859bf66fc0
Stackbit Code Editor <projects@stackbit.com>
2021-06-16 14:43:43 +0000

    ContactSection.js: changed

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -30,7 +30,7 @@ export default class ContactSection extends React.Component {
                         </div>
                         <div className="form-group form-checkbox">
                             <input aria-labelledby="consent-label" type="checkbox" name="consent" id="consent" />
-                            <label id="consent-label" htmlFor="consent">I understand that this form is storing my submitted information so I can be contacted.</label>
+                            <label id="consent-label" htmlFor="consent">Entiendo que este formulario está almacenando mi información enviada para que pueda ser contactado.</label>
                         </div>
                         <div className="form-submit">
                             <button type="submit" className="button">Enviar mensaje</button>
