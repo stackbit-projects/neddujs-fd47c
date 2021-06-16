0bd01f7cc6ca0a8aacfa50b43e639cc0c8110b01
Stackbit Code Editor <projects@stackbit.com>
2021-06-16 14:42:25 +0000

    ContactSection.js: changed

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -26,7 +26,7 @@ export default class ContactSection extends React.Component {
                         </div>
                         <div className="form-group">
                             <label id="message-label" htmlFor="message">Mensaje</label>
-                            <textarea aria-labelledby="message-label" name="message" id="message" rows="5" placeholder="Your message" />
+                            <textarea aria-labelledby="message-label" name="message" id="message" rows="5" placeholder="Tu message" />
                         </div>
                         <div className="form-group form-checkbox">
                             <input aria-labelledby="consent-label" type="checkbox" name="consent" id="consent" />
