7b5e64aa570e9054cd03e3b377a676e6e20e794f
Stackbit Code Editor <projects@stackbit.com>
2021-06-16 14:41:39 +0000

    ContactSection.js: changed

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -18,7 +18,7 @@ export default class ContactSection extends React.Component {
                         <input aria-labelledby="honeypot-label" type="hidden" name="form-name" value="contactForm" />
                         <div className="form-group">
                             <label id="name-label" htmlFor="name">Nombre</label>
-                            <input aria-labelledby="name-label" type="text" name="name" id="name" placeholder="Your name" required />
+                            <input aria-labelledby="name-label" type="text" name="name" id="name" placeholder="Tu nombre" required />
                         </div>
                         <div className="form-group">
                             <label id="email-label" htmlFor="email">Email</label>
